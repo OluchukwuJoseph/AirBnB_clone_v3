6e2fef75c456c77f667bda5b05754e968aa17fa3
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-08-20 12:37:12 +0100

    Resolved issues in Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-The first version of AirBnB clone Flask app
-"""
+""" The first version of AirBnB clone Flask app """
 from api.v1.views import app_views
 from flask import Flask, jsonify
 from models import storage
